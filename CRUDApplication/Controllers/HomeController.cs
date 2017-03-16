25c7cd5148f1f6a469bb8396687e7fe7eb4f3924
Đỗ Mạnh Hà < Mnh H>
2017-03-17 01:39:46 +0700

    Creating CRUD App using ASP.NET MVC & Entity Framework 6.x

---
+++ CRUDApplication/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CRUDApplication.Models;
+
+namespace CRUDApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetEmployees()
+        {
+            using (CRUDApplicationEntities dc = new CRUDApplicationEntities())
+            {
+                var employees = dc.Employees.OrderBy(a => a.FirstName).ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using(CRUDApplicationEntities dc = new CRUDApplicationEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Employee e)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (CRUDApplicationEntities dc = new CRUDApplicationEntities())
+                {
+                    if (e.EmployeeID > 0)
+                    {
+                        // Edit
+                        var v = dc.Employees.Where(a => a.EmployeeID == e.EmployeeID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.FirstName = e.FirstName;
+                            v.LastName = e.LastName;
+                            v.EmailID = e.EmailID;
+                            v.City = e.City;
+                            v.Country = e.Country;
+                        }
+                    } else
+                    {
+                        // Add
+                        dc.Employees.Add(e);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (CRUDApplicationEntities dc = new CRUDApplicationEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                } else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteEmployee(int id)
+        {
+            bool status = false;
+            using(CRUDApplicationEntities dc = new CRUDApplicationEntities())
+            {
+                var v = dc.Employees.Where(a => a.EmployeeID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Employees.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ CRUDApplication/Models/Extended/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUDApplication.Models
+{
+    public partial class Employee
+    {
+
+    }
+
+    public class EmployeeMetadata
+    {
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please provide first name")]
+        public string FirstName { get; set; }
+
+        [Required (AllowEmptyStrings = false, ErrorMessage = "Please provide last name")]
+        public string LastName { get; set; }
+    }
+}
